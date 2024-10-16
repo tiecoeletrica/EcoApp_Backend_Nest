4265591882b73cc66e107b4b6ed9b144d9b631b1
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-10-16 07:38:20 -0300

    request's userId shows on console.log

--- src/infra/request-logger.middleware.ts
+++ src/infra/request-logger.middleware.ts
@@ -1,18 +1,41 @@
 // src/request-logger.middleware.ts
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+import { jwtDecode } from "jwt-decode";
+import { UserPayload } from "./auth/jwt-strategy.guard";
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-    // console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
+    let userId = "Unknown";
+
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      const token = authHeader.substring(7);
+      try {
+        const decoded = jwtDecode<UserPayload>(token);
+        if (decoded && decoded.sub) {
+          userId = decoded.sub;
+        }
+      } catch (error) {
+        console.error("Error decoding JWT:", error);
+      }
+    }
+
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}
+      User ID: ${userId}
+      Body: ${JSON.stringify(req.body, null, 2)}`
+    );
 
     const start = Date.now();
-    res.on('finish', () => {
+    res.on("finish", () => {
       const duration = Date.now() - start;
-      console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`);
+      console.log(
+        `[${new Date().toISOString()}] ${req.method} ${req.originalUrl} ${
+          res.statusCode
+        } ${duration}ms`
+      );
     });
 
     next();
